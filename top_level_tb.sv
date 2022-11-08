20665691f3b317b95eab0d05ba6334d1074d406b
bryan-kwan <bryan.kwan.mail@gmail.com>
2022-11-08 11:35:41 -0700

    Updated top_level testbench for new functionalities

--- top_level_tb.sv
+++ top_level_tb.sv
@@ -13,16 +13,35 @@ module top_level_tb();
 
 	logic [7:0] switch_value;
 
+	logic buzzer;
+	logic pwm_out_LEDR, pwm_out_flash, pwm_out_buzzer;
+
+	parameter WIDTH=13,MAX_COUNT=3000, // LEDR parameters
+	// Display flashing / buzzer parameters
+	BASE_PERIOD_flash = 2000, // Number of clock cycles for period of pwm_out by defeault (with enable=1)
+	DUTY_CYCLE_flash = 1000, // duty cycle = BASE_PERIOD / DUTY_CYCLE, 50% by default
+	MAX_FLASH_DISTANCE = 2000, // Distance (in 10^-2 cm) at which flashing starts
+	MIN_FLASH_DISTANCE = 0, 
+	CLOCK_DIVIDE_LOW_FREQ_flash = 25000, // Period = (1/50MHz) * BASE_PERIOD * CLOCK_DIVIDE
+	CLOCK_DIVIDE_HIGH_FREQ_flash = 5000,
+	BASE_PERIOD_buzzer = 100, 
+	DUTY_CYCLE_buzzer = 50, 
+	CLOCK_DIVIDE_LOW_FREQ_buzzer = 2040,
+	CLOCK_DIVIDE_HIGH_FREQ_buzzer = 40,
+	DISTANCE2CLOCK_DIVIDE_SCALING_flash = (CLOCK_DIVIDE_LOW_FREQ_flash-CLOCK_DIVIDE_HIGH_FREQ_flash)
+                                            / (MAX_FLASH_DISTANCE-MIN_FLASH_DISTANCE),
+	DISTANCE2CLOCK_DIVIDE_SCALING_buzzer = (CLOCK_DIVIDE_LOW_FREQ_buzzer-CLOCK_DIVIDE_HIGH_FREQ_buzzer)
+                                            / (MAX_FLASH_DISTANCE-MIN_FLASH_DISTANCE);
+	int n_cycles;
+
 	parameter CLOCK_PERIOD = 20;
 	parameter delay = 6*CLOCK_PERIOD;
 	parameter bcd_delay = 250*CLOCK_PERIOD;
 	parameter debouncer_stable_time = 5000000*CLOCK_PERIOD;
 
-	parameter N_SAMPLES = 256;
-	parameter SAMPLE_WIDTH = 16;
 
 	static int use_force_button_tests = 1; // 1 = use force to change write_enable, 0 = use button
-	static int counter; // used as an intermediate variable for force statement
+	static int distance_temp; // used as an intermediate variable for force statement
 	// Instantiate UUTs
 	//(input  logic       clk,
 	//  input  logic       reset_n,
@@ -34,133 +53,150 @@ module top_level_tb();
 	.SW(SW),.LEDR(LEDR), .button(button),
 	.HEX0(HEX0),.HEX1(HEX1),
 	.HEX2(HEX2),.HEX3(HEX3),
-	.HEX4(HEX4),.HEX5(HEX5));
+	.HEX4(HEX4),.HEX5(HEX5), 
+	.buzzer(buzzer));
 
-	// Function to check correct seven segment value for a given number
-	// Returns 1 if segment is correct, else returns 0
-	function int segment_is_correct(int digit, logic[6:0] display);
-		segment_is_correct=1;
-		case(digit)
-				0: assert(display[6:0]===~7'b0_11_11_11) else segment_is_correct=0;
-				1: assert(display[6:0]===~7'b0_00_01_10) else segment_is_correct=0;
-				2: assert(display[6:0]===~7'b1_01_10_11) else segment_is_correct=0;
-				3: assert(display[6:0]===~7'b1_00_11_11) else segment_is_correct=0;
-				4: assert(display[6:0]===~7'b1_10_01_10) else segment_is_correct=0;
-				5: assert(display[6:0]===~7'b1_10_11_01) else segment_is_correct=0;
-				6: assert(display[6:0]===~7'b1_11_11_01) else segment_is_correct=0;
-				7: assert(display[6:0]===~7'b0_00_01_11) else segment_is_correct=0;
-				8: assert(display[6:0]===~7'b1_11_11_11) else segment_is_correct=0;
-				9: assert(display[6:0]===~7'b1_10_01_11) else segment_is_correct=0;
-				10: assert(display[6:0]===~7'b1_11_01_11) else segment_is_correct=0;
-				11: assert(display[6:0]===~7'b1_11_11_00) else segment_is_correct=0;
-				12: assert(display[6:0]===~7'b0_11_10_01) else segment_is_correct=0;
-				13: assert(display[6:0]===~7'b1_01_11_10) else segment_is_correct=0;
-				14: assert(display[6:0]===~7'b1_11_10_01) else segment_is_correct=0;
-				15: assert(display[6:0]===~7'b1_11_00_01) else segment_is_correct=0;
-		endcase
-	endfunction
 	// Apply stimulus
 	always #(CLOCK_PERIOD/2) clk = ~clk; // run clock forever with period CLOCK_PERIOD ns 
 	
 	assign write_enable = UUT.register_ins.write_enable; // Probes for viewing signals
 	assign voltage = UUT.MUX_binary_output_ins.in2;
-	assign distance = UUT.MUX_binary_output_ins.in1;
+	assign distance = UUT.distance;
 	assign avg_out = UUT.MUX_hexadecimal_output_ins.in2;
 	assign reg_out = UUT.reg_out;
 	assign DP=UUT.DP_in;
 	assign Blank=UUT.Blank;
+	assign pwm_out_LEDR = ~UUT.pwm_led; // Outputs are the inversion of the signals we want to check
+	assign pwm_out_flash = ~UUT.pwm_flash;
+	assign pwm_out_buzzer = ~UUT.pwm_buzzer;
 	
 	initial begin 
 		$display("---  Testbench started  ---");
 		
-		#(0.25*CLOCK_PERIOD); // Offset the stimulus by 0.25 Period
+		//#(0.25*CLOCK_PERIOD); // Offset the stimulus by 0.25 Period
 
 		if(!use_force_button_tests) begin
 			button=0; #(delay);
 			button=1; #(debouncer_stable_time); // Enable on
 		end else force UUT.write_enable=1;
 
-		// Reset tests
+		// Reset test ==================
+		$display("Testing reset, time=%t ps",$time);
 		SW[9:8]=2'b00; // Set to decimal mode
 		reset_n = 1; #(delay);
 		reset_n = 0;#(delay); // Reset is active low
+		assert(UUT.reg_out==0) else $error("Expected reg_out=0 received %b", UUT.reg_out);
 		reset_n = 1; #(delay);
 		
 		SW[9:8]=2'b01; // Set to voltage mode
 		reset_n = 1; #(delay);
 		reset_n = 0; #(delay); // Reset is active low
+		assert(UUT.reg_out==0) else $error("Expected reg_out=0 received %b", UUT.reg_out);
 		reset_n = 1; #(delay);
 		
 		SW[9:8]=2'b10; // Set to distance value mode
 		reset_n = 1; #(delay);
 		reset_n = 0; #(delay); // Reset is active low
+		assert(UUT.reg_out==0) else $error("Expected reg_out=0 received %b", UUT.reg_out);
 		reset_n = 1; #(delay);
 		
 		SW[9:8]=2'b11; // Set to average mode
 		reset_n = 1; #(delay);
 		reset_n = 0; #(delay); // Reset is active low
+		assert(UUT.reg_out==0) else $error("Expected reg_out=0 received %b", UUT.reg_out);
 		reset_n = 1; #(delay);
-		
-		// Display value tests
+		$display("Finished testing reset, time=%t ps",$time);
 
-		SW[9:8]=2'b00; #(delay) // Set to hexadecimal mode
-		$display("Start of hexadecimal mode test (time=%t ps)", $time);
-		assert(DP===6'b00_0000) else $error("Hexadecimal mode: Expected DP=6'b00_0000 (received %b)",DP);
-		assert(Blank===6'b11_0000) else $error("Hexadecimal mode: Expected Blank=6'b11_0000 (received %b)",Blank);
-		for (int i = 0; i < 256; i++) begin
-			SW[7:0]=i; #(bcd_delay);
-			digit0 = i / 16 ** (1 - 1) % 16; // First digit of i in base 16
-			digit1= i / 16 **(2 - 1) % 16; // Second digit
-			digit2= i / 16 ** (3 - 1) % 16; // Third digit
-			digit3= i / 16 ** (4 - 1) % 16; // Fourth digit
-			if(segment_is_correct(digit0, HEX0)===0) $error("Hexadecimal mode failed for value %d in HEX0, Num_Hex0=%d",i, UUT.Num_Hex0);
-			if(segment_is_correct(digit1, HEX1)===0) $error("Hexadecimal mode failed for value %d in HEX1, Num_Hex1=%d",i, UUT.Num_Hex1);
-			if(segment_is_correct(digit2, HEX2)===0) $error("Hexadecimal mode failed for value %d in HEX2, Num_Hex2=%d",i, UUT.Num_Hex2);
-		end
-		$display("End of hexadecimal mode test");
+		// PWM Signal tests
+		SW[9:8]=2'b10; // Distance mode for convenience (pwm signals are on in all modes)
+		distance_temp=1;
+		force UUT.ADC_Data_ins.distance=distance_temp;
+		// LEDR duty cycle test ==================
+		$display("Testing LEDR duty cycle, time=%t ps",$time);
+        // Reset
+        reset_n = 1; #(CLOCK_PERIOD);
+        reset_n = 0; #(CLOCK_PERIOD);
+        reset_n = 1;
+        // Distance greater than max distance MAX_COUNT: pwm_out_LEDR should always be 1
+        // Distance less than max distance MAX_COUNT: pwm_out_LEDR=1 for distance clock cycles and 0 for rest
+        for(int i = 0; i<2**(WIDTH-1)-1; i+=500) begin
+			distance_temp = i;
+            for(int j = 1; j<MAX_COUNT; j++) begin // Check every clock cycle until counter resets to 0
+                #(CLOCK_PERIOD);
+                if(distance>=MAX_COUNT)
+                    assert(pwm_out_LEDR===1'b1) else $error("Expected pwm_out_LEDR=1 received pwm_out_LEDR=%b (Applied distance=%d, count=%d)",pwm_out_LEDR,distance,j);
+                else if(j<distance)
+                    assert(pwm_out_LEDR===1'b1) else $error("Expected pwm_out_LEDR=1 received pwm_out_LEDR=%b (Applied distance=%d, count=%d)",pwm_out_LEDR,distance,j);
+                else
+                    assert(pwm_out_LEDR===1'b0) else $error("Expected pwm_out_LEDR=0 received pwm_out_LEDR=%b (Applied distance=%d, count=%d)",pwm_out_LEDR,distance,j);
+            end
+            // Reset
+            reset_n = 1; #(CLOCK_PERIOD);
+            reset_n = 0; #(CLOCK_PERIOD);
+            reset_n = 1;
+        end
+        $display("Finished testing LEDR duty cycle, time=%t ps",$time);
+		// Flash period test ==================
+		$display("Testing flash period, time=%t ps",$time);
+        // Reset
+        reset_n = 1; #(CLOCK_PERIOD);
+        reset_n = 0; #(CLOCK_PERIOD);
+        // Period = BASE_PERIOD * (1/50MHz) * (distance * DISTANCE2CLOCK_DIVIDE_SCALING)
+        for(int i = 0; i<1.1*MAX_FLASH_DISTANCE; i+=1000) begin
+            $display("Applying distance = %d", i);
+			distance_temp = i; #(CLOCK_PERIOD);
+            reset_n = 1;
+            if(distance>MIN_FLASH_DISTANCE & distance<MAX_FLASH_DISTANCE) 
+                n_cycles = BASE_PERIOD_flash * (distance * DISTANCE2CLOCK_DIVIDE_SCALING_flash);
+            else
+                n_cycles = BASE_PERIOD_flash;
+            //pwm_out is 1 for 50%, then 0 for 50%
+            assert(pwm_out_flash===1'b1) else $error("Expected pwm_out_flash=1 received pwm_out_flash=%b (Applied distance=%d)",pwm_out_flash,distance);
+            #((n_cycles-1)*CLOCK_PERIOD);
+            assert(pwm_out_flash===1'b0) else $error("Expected pwm_out_flash=0 received pwm_out_flash=%b (Applied distance=%d)",pwm_out_flash,distance);
+            #(3*CLOCK_PERIOD);
+            assert(pwm_out_flash===1'b1) else $error("Expected pwm_out_flash=1 received pwm_out_flash=%b (Applied distance=%d)",pwm_out_flash,distance);
+            
+            // Reset
+            reset_n = 1; #(CLOCK_PERIOD);
+            reset_n = 0; #(CLOCK_PERIOD);
+        end
+        $display("Finished testing flash period, time=%t ps",$time);
+		// Buzzer period test ==================
+		$display("Testing buzzer period, time=%t ps",$time);
+        // Reset
+        reset_n = 1; #(CLOCK_PERIOD);
+        reset_n = 0; #(CLOCK_PERIOD);
+        // Period = BASE_PERIOD * (1/50MHz) * (distance * DISTANCE2CLOCK_DIVIDE_SCALING)
+        for(int i = 0; i<1.1*MAX_FLASH_DISTANCE; i+=500) begin
+            $display("Applying distance = %d", i);
+			distance_temp = i; #(CLOCK_PERIOD);
+            reset_n = 1;
+            if(distance>MIN_FLASH_DISTANCE & distance<MAX_FLASH_DISTANCE) 
+                n_cycles = BASE_PERIOD_buzzer * (distance * DISTANCE2CLOCK_DIVIDE_SCALING_buzzer);
+            else
+                n_cycles = BASE_PERIOD_buzzer;
+            //pwm_out is 1 for 50%, then 0 for 50%
+            assert(pwm_out_buzzer===1'b1) else $error("Expected pwm_out_buzzer=1 received pwm_out_buzzer=%b (Applied distance=%d)",pwm_out_buzzer,distance);
+            #((n_cycles-1)*CLOCK_PERIOD);
+            assert(pwm_out_buzzer===1'b0) else $error("Expected pwm_out_buzzer=0 received pwm_out_buzzer=%b (Applied distance=%d)",pwm_out_buzzer,distance);
+            #(3*CLOCK_PERIOD);
+            assert(pwm_out_buzzer===1'b1) else $error("Expected pwm_out_buzzer=1 received pwm_out_buzzer=%b (Applied distance=%d)",pwm_out_buzzer,distance);
+            
+            // Reset
+            reset_n = 1; #(CLOCK_PERIOD);
+            reset_n = 0; #(CLOCK_PERIOD);
+        end
+        $display("Finished testing buzzer period, time=%t ps",$time);
 
-		// Average mode tests
-		$display("Start of average mode test (time=%t ps)", $time);
-		SW[9:8]=2'b01; #(delay); // Set to average mode
-		assert(DP===6'b00_0000) else $error("Average mode: Expected DP=6'b00_0000 (received %b)",DP);
-		force UUT.ADC_Data_ins.ADC_out = 100; #(delay);
-		assert(reg_out==UUT.ADC_Data_ins.ADC_out) else $error("Average mode: Expected reg_out=%b (received %b)",UUT.ADC_Data_ins.ADC_out,reg_out);
-		assert(Blank===6'b11_1100) else $error("Average mode: Expected Blank=%b (recieved %b)",6'b11_1100,Blank);
-		release UUT.ADC_Data_ins.ADC_out;
-		$display("End of average mode test");
+		release UUT.ADC_Data_ins.distance;
 
-		// Distance mode tests
-		$display("Start of distance mode test (time=%t ps)", $time);
-		SW[9:8]=2'b10; #(bcd_delay); // Set to distance mode
-		assert(DP===6'b00_0100) else $error("Distance mode: Expected DP=6'b00_0100 (received %b)",DP);
-		assert(reg_out==UUT.distance) else $error("Distance mode: Expected reg_out=%b (received %b)",distance,reg_out);	
-		// Check value pair from v2d_rom.txt: voltage=100, distance=26A
-		force UUT.ADC_Data_ins.voltage_temp = 100; #(delay);
-		assert(distance=='h26A) else $error("Distance mode: Expected distance=%h (recieved %h)",'h26A,distance);
-		assert(Blank===6'b11_1000) else $error("Distance mode: Expected Blank=%b (recieved %b)",6'b11_1000,Blank);
-		release UUT.ADC_Data_ins.voltage_temp;
-		$display("End of distance mode test");
-		
-		// Voltage mode tests
-		$display("Start of voltage mode test (time=%t ps)", $time);
-		SW[9:8]=2'b11; #(bcd_delay); // Set to voltage mode
-		assert(DP===6'b00_1000) else $error("Voltage mode: Expected DP=6'b00_1000 (received %b)",DP);
-		assert(Blank===6'b11_0000) else $error("Voltage mode: Expected Blank=6'b11_0000 (received %b)",Blank);
-		// Override the internal signal for average to check correct voltage conversion
-		for(int i=0; i<4096; i++) begin
-			counter=i;
-			force UUT.ADC_Data_ins.ADC_out_ave=counter; #(delay); 
-			// Formula: voltage_temp = ADC_out_ave*2500*2/(2**12);
-			assert(voltage==i*2500*2/(2**12)) else $error("Voltage mode: Expected voltage=%d (received %d)",i*2500*2/(2**12),voltage);
-			release UUT.ADC_Data_ins.ADC_out_ave;
-		end
-		$display("End of voltage mode test");
-
-		// Freeze button tests
+		// Freeze button test ==================
 		$display("Start of freeze button test (time=%t ps)", $time);
+		reset_n = 1; #(CLOCK_PERIOD);
 		SW[9:8]=2'b00; #(delay);
 		SW[7:0]='1;
-		switch_value=SW[7:0];
+		switch_value=SW[7:0]; #(delay);
 		if(!use_force_button_tests) begin
 			button=0; #(debouncer_stable_time); // Enable off
 		end
